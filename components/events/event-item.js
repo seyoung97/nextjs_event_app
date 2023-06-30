50aa4de195dad455c0c4ecd230d2a7b008307745
seyoung97 <seyoung1st@gmail.com>
2023-06-30 17:52:33 +0900

    Design: icons 추가

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,5 +1,8 @@
 import Link from "next/link";
 import Button from "../ui/button";
+import DateIcon from '../icons/date-icon'
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon"
 
 import classes from "./event-item.module.css";
 
@@ -22,14 +25,19 @@ function EventItem(props) {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon/>
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Button link={exploreLink}>Explore Event</Button>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}><ArrowRightIcon/></span>
+            </Button>
         </div>
       </div>
     </li>
