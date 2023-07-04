14fb5a5dfed3049082b926e2b6aa3cfbfc24717b
seyoung97 <seyoung1st@gmail.com>
2023-07-04 12:07:57 +0900

    Design: 필터 UI 구현

--- components/ui/button.js
+++ components/ui/button.js
@@ -1,11 +1,21 @@
 import Link from "next/link";
 
-import classes from './button.module.css'
+import classes from "./button.module.css";
 
-function Button (props) {
+function Button(props) {
+  if (props.link) {
     return (
-        <Link href={props.link} className={classes.btn}>{props.children}</Link>
-    )
+      <Link href={props.link} className={classes.btn}>
+        {props.children}
+      </Link>
+    );
+  }
+
+  return (
+    <button className={classes.btn} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
 }
 
 export default Button;
